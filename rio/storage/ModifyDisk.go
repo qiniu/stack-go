0565a02ed6d0934d30291e76dcc1ca81c0c35854
liuzixuan <1479780500@qq.com>
2021-07-20 12:16:34 +0800

    disk-o-delete_test

--- rio/storage/ModifyDisk.go
+++ rio/storage/ModifyDisk.go
@@ -10,18 +10,11 @@ import (
 
 // ModifyDiskArgs 修改磁盘参数
 type ModifyDiskArgs struct {
-	ZoneID string `json:"zone_id"`
-
-	DiskID string `json:"disk_id"`
-
-	// 磁盘名称。长度为2~128个英文或中文字符。必须以大小字母或中文开头，不能以 `http://` 和 `https://` 开头。可以包含数字、半角冒号（:）、下划线（_）或者连字符（-）。
-	DiskName *string `json:"disk_name,omitempty"`
-
-	// 磁盘描述。 长度为2~256个英文或中文字符，不能以 `http://` 和 `https://` 开头。
-	Description *string `json:"description,omitempty"`
-
-	// 磁盘是否随实例释放。默认值：无，无表示不改变当前的值。
-	DeleteWithServer *bool `json:"delete_with_server,omitempty"`
+	ZoneID           string  `json:"zone_id"`
+	DiskID           string  `json:"disk_id"`
+	DiskName         *string `json:"disk_name,omitempty"`
+	Description      *string `json:"description,omitempty"`
+	DeleteWithServer *bool   `json:"delete_with_server,omitempty"`
 }
 
 // ModifyDiskResp 修改磁盘返回
