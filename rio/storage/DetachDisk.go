53276c5f7b8a4a6404e570172b5178eb8c97f2b5
liuzixuan <1479780500@qq.com>
2021-07-21 19:36:02 +0800

    DiskAPI

--- rio/storage/DetachDisk.go
+++ rio/storage/DetachDisk.go
@@ -8,19 +8,19 @@ import (
 	"github.com/qiniu/stack-go/rio/common"
 )
 
-//DetachDiskArgs 解绑磁盘参数
+// DetachDiskArgs 解绑磁盘参数
 type DetachDiskArgs struct {
 	ZoneID   string `json:"zone_id"`
 	DiskID   string `json:"disk_id"`
 	ServerID string `json:"server_id"`
 }
 
-//DetachDiskResp 解绑磁盘返回
+// DetachDiskResp 解绑磁盘返回
 type DetachDiskResp struct {
 	common.Response
 }
 
-//DetachDisk 解绑磁盘
+// DetachDisk 解绑磁盘
 func (d *Disk) DetachDisk(args *DetachDiskArgs) (resp *DetachDiskResp, err error) {
 	url := fmt.Sprintf("%s/disk/%s/detach", StorageURLPrefix, args.DiskID)
 	req := client.NewRequest(http.MethodPost, url).WithJSONBody(args).WithZoneID(&args.ZoneID)
